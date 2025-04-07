6363a33f0a52ff67fe65e21f25a85da410a78da5
Victor Olofsson <vicolo-9@student.ltu.se>
2025-04-07 11:10:16 +0200

    Just updated frontend dependencies
    Signed-off-by: Victor Olofsson <vicolo-9@student.ltu.se>

--- src/main/java/org/example/tornedalen/TornedalenApplication.java
+++ src/main/java/org/example/tornedalen/TornedalenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TornedalenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TornedalenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TornedalenApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/tornedalen/TornedalenApplicationTests.java
+++ src/test/java/org/example/tornedalen/TornedalenApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TornedalenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
