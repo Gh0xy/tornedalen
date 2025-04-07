de23f867c1945d88453167323a7f5727fd709b09
Victor Olofsson <vicolo-9@student.ltu.se>
2025-04-07 10:39:48 +0200

    Initial commit

---
+++ src/main/java/org/example/tornedalen/TornedalenApplication.java
@@ -0,0 +1,13 @@
+package org.example.tornedalen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornedalenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornedalenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tornedalen/TornedalenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tornedalen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornedalenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
