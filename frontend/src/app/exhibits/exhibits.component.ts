9c2ecac55ab308162661f5182814767308b8c245
Emil Hörnsten <hornstenemil@gmail.com>
2025-04-15 12:58:36 +0200

    Första push

---
+++ frontend/src/app/exhibits/exhibits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibits',
+  imports: [],
+  templateUrl: './exhibits.component.html',
+  styleUrl: './exhibits.component.sass'
+})
+export class ExhibitsComponent {
+
+}
---
+++ frontend/src/app/quiz/quiz.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  imports: [],
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.component.sass'
+})
+export class QuizComponent {
+
+}
