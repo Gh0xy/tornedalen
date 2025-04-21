30e946c9d3446455c92fff7ba80c2fa59f6032ca
Brandon Liu <164882775+Braandonl1@users.noreply.github.com>
2025-04-21 13:49:49 +0200

    routes till alla delar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'quiz',
+    loadComponent: () =>
+      import('./quiz/quiz.component').then((m) => m.QuizComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./contact/contact.component').then((m) => m.ContactComponent),
+  },
+  {
+    path: 'discussion',
+    loadComponent: () =>
+      import('./discussion/discussion.component').then((m) => m.DiscussionComponent),
+  },
+  {
+    path: 'exhibits',
+    loadComponent: () =>
+      import('./exhibits/exhibits.component').then((m) => m.ExhibitsComponent),
+  },
+];
