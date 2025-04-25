1d6fa1b80ac03372c938a953b15a1b90cd3ec2d0
Emil Hörnsten <hornstenemil@gmail.com>
2025-04-25 11:26:02 +0200

    ny komponent: svara på inlägg

---
+++ frontend/src/app/pages/reply-to-post/reply-to-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-to-post',
+  imports: [],
+  templateUrl: './reply-to-post.component.html',
+  styleUrl: './reply-to-post.component.css'
+})
+export class ReplyToPostComponent {
+
+}
